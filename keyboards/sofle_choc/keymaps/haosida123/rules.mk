a86772eea2a555b1cf7c105c7a876acf4f3f54cb
Sida Hao <haosida123@gmail.com>
2025-03-25 17:16:27 -0500

    layouts

--- keyboards/sofle_choc/keymaps/haosida123/rules.mk
+++ keyboards/sofle_choc/keymaps/haosida123/rules.mk
@@ -1,15 +1,23 @@
-# TRI_LAYER_ENABLE = yes
+VIA_ENABLE = yes
+
+TRI_LAYER_ENABLE = yes
 
 ENCODER_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
 
-OLED_ENABLE = yes
+# OLED_ENABLE = yes
+OLED_ENABLE = no
 
 # Enable per-key lighting
-RGB_MATRIX_ENABLE = yes
+# RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_ENABLE = no
+
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
 
 # Enable audio keys and system control
 EXTRAKEY_ENABLE = yes
+MOUSEKEY_ENABLE = yes
 
 # Enable link time optimization
 # Trade longer compile time for smaller firmware size
@@ -17,9 +25,8 @@ LTO_ENABLE = yes
 
 
 
-
-
-
 SPACE_CADET_ENABLE = no
 GRAVE_ESC_ENABLE = no
 MAGIC_ENABLE = no # uncomment if you're not using magic keys
+
+MUSIC_ENABLE = no
